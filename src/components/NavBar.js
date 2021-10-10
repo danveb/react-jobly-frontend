7f846ddce7dac3148f9a8eaa489790ca1bb5b04b
Daniel Bae <danibae91@icloud.com>
2021-10-10 12:46:25 -0700

    update Navbar for client-side routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,37 @@
 import React from "react"
-import "../static/styles/NavBar.css"
-// import { NavLink } from "react-router-dom"
-import { Nav, NavItem, NavLink } from 'reactstrap';
+// import "../static/styles/NavBar.css"
+import { Link } from "react-router-dom"
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap'
 
 const NavBar = () => {
     return (
         <div>
-            <Nav>
-                <NavItem>
-                    <NavLink href="/">Jobly</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/companies">Companies</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/jobs">Jobs</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/login">Login</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/profile">Profile</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/signup">Signup</NavLink>
-                </NavItem>
-                {/* <NavItem>
-                    <NavLink disabled href="#">Disabled Link</NavLink>
-                </NavItem> */}
-            </Nav>  
+            <Navbar color="light" light expand="lg">
+                <NavbarBrand tag={Link} exact to="/">Jobly</NavbarBrand>
+                <Nav className="mr-auto" navbar>
+                    <NavItem>
+                    <NavLink tag={Link} exact to="/companies">Companies</NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink tag={Link} exact to="/jobs">Jobs</NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink tag={Link} exact to="/login">Login</NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink tag={Link} exact to="/profile">Profile</NavLink>
+                    </NavItem>
+                    <NavItem>
+                    <NavLink tag={Link} exact to="/signup">Signup</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
         </div>
     )
 }
