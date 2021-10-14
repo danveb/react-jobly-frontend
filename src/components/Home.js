337ab923faed94e3a0d1f25b257216998de964af
Daniel Bae <danibae91@icloud.com>
2021-10-14 01:46:00 -0700

    implement client-side routing for login & signup buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react' 
+import { Link } from 'react-router-dom'
 import { Container, Row, Col, Button } from 'reactstrap'
 import '../static/styles/Home.css'
 
@@ -8,8 +9,8 @@ const Home = () => {
             <Container className="themed-container">
                 <h1>Jobly</h1>
                 <p>All the jobs in one, convenient place.</p>
-                <Button color="primary" href="/login">Log In</Button>{' '}
-                <Button color="primary" href="/signup">Sign Up</Button>{' '}
+                <Button color="primary" tag={Link} to="/login">Log In</Button>{' '}
+                <Button color="primary" tag={Link} to="/signup">Sign Up</Button>{' '}
             </Container>
             
         </div>
