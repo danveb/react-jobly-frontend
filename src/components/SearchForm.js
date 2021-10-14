3c937491f98adb9fa676dd4a118776b5537a5a31
Daniel Bae <danibae91@icloud.com>
2021-10-14 12:13:16 -0700

    continue refactoring SearchForm

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,37 +1,48 @@
 import React, { useState } from 'react' 
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap' 
+import '../static/styles/SearchForm.css' 
+
 
 const SearchForm = () => {
     // initialize INITIAL_STATE obj
     const INITIAL_STATE = {
-        
+        search: ''
     }
     const [formData, setFormData] = useState(INITIAL_STATE) 
 
-    handleChange = (e) => {
+    const handleChange = (e) => {
         // { name, val } e.target
-        const { name, val } = e.target 
-        setFormData({
+        const { name, value } = e.target 
+        setFormData(formData => ({
             ...formData, 
-            [name]: val 
-        })
+            [name]: value 
+        }))
     }
 
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault() 
+        console.log('submitted') 
     }
 
     return (
-        <form>
-            <label>Search Form</label>
-            <input 
+        <Form onSubmit={handleSubmit}>
+            <Label htmlFor="search"></Label>
+            <FormGroup>
+            <Input 
+                id="search"
                 type="text"
-                name="search-form"
-                value={formData.value} 
+                name="search"
                 placeholder="Enter search term..."
+                value={formData.value} 
                 onChange={handleChange}
             
             />
-        </form>
+            </FormGroup>
+
+            <FormGroup>
+                <Button className="btn-submit" color="primary">Submit</Button>{' '}
+            </FormGroup>
+        </Form>
     )
 }
 
