2ef56a1905fb62c8d83450b8a81936d13c362ba2
Daniel Bae <danibae91@icloud.com>
2021-10-15 16:02:54 -0700

    create JobCard component

---
+++ src/components/JobCard.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Button, Card, CardBody, CardText, CardTitle, InputGroupAddon } from "reactstrap"
+import "../static/styles/JobCard.css"
+
+const JobCard = ({ title, salary, equity, companyName }) => {
+    return (
+        <div>
+        <Card className="JobCard-tile">
+            <CardBody>
+                <CardTitle tag="h6">{title}</CardTitle>
+                <CardText>{companyName}</CardText>
+                <CardText>{`Salary: $${Number(salary).toLocaleString()} | Equity: ${equity === null ? 0 : equity}%`}</CardText>
+                <InputGroupAddon addonType="append">
+                <Button color="danger">Apply</Button>
+                </InputGroupAddon>
+            </CardBody>
+        </Card>
+        </div>
+    )
+}
+
+export default JobCard
